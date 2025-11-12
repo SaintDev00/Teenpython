02bb9b3603d7974e7886d40d28d766e1afb78e82
Saint Dev <jqydjdj@gmail.com>
2025-11-11 21:43:40 -0500

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,20 @@
+Precio_pan = 300
+
+cantidad=int(input("Ingrese la cantidad de panes"))
+
+if cantidad <0:
+    print("cantidad invalida")
+subtotal = cantidad*Precio_pan
+descuento = 0
+porcentaje = 0
+if cantidad >20 and cantidad <=50:
+    descuento=subtotal*0.10
+    descuento_2=subtotal-porcentaje
+    print(F"El precio es: {descuento_2}")
+    print(F"El descuento es: {descuento}")
+
+elif cantidad >50:
+    descuento=subtotal *0.20 
+    descuento_1=subtotal-descuento
+    print(f"El precio es: {descuento_1}")
+    print(F"El descuento es: {descuento}")
---
+++ ex2.py
@@ -0,0 +1,7 @@
+age=int(input("Give me your age"))
+
+if age <0:
+    print("Error.202")
+
+elif 5<=age >=5
+
