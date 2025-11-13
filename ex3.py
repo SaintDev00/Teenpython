25d67856b66b46ae35dbf9a0d3d139cb89b7ade8
Saint Dev <jqydjdj@gmail.com>
2025-11-12 21:53:38 -0500

    Add gym discipline feedback based on input days

---
+++ ex3.py
@@ -0,0 +1,11 @@
+iscipline=input("How much days do you go to the gym?\n")
+
+if Discipline>=4:
+    print("¡Excelente disciplina!You won one energy point")
+
+elif Discipline >2 and Discipline<=3:
+    print("Good,but you are more strong")
+
+elif Discipline >0 and Discipline <=1:
+    print("Dont give up,you can improve")
+    
