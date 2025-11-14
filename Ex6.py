d67e8088208c9ed02b5033b8e5f70477909ea56c
Saint Dev <jqydjdj@gmail.com>
2025-11-13 21:38:21 -0500

    Implement rapidcar billing calculation logic

---
+++ Ex6.py
@@ -0,0 +1,17 @@
+Rapidcar=int(input("Ingrese el n de horas? \n" ))
+Ceroacinco = 2000
+Multafija = 5000
+multiplicacion = Ceroacinco *  Rapidcar
+multa = multiplicacion + Multafija
+while True:
+   
+    Rapidcar
+    if Rapidcar >0 and Rapidcar<=5:
+                print(f"El calculo es: {multiplicacion}" )
+                break
+    elif Rapidcar >=5:
+            print(f"El valor de su factura es {multa} porque se paso del tiempo")
+            break
+    else:
+        print("Numero invalido ingrese un numero entero valido")                      
+        break
