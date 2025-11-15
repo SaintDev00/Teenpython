08fd2c8f4e47928fc7b7d7037ffcd8577f939acf
Saint Dev <jqydjdj@gmail.com>
2025-11-14 19:44:38 -0500

    Add student discount and coupon functionality

--- Ex5.py
+++ Ex5.py
@@ -1,2 +1,20 @@
 Libro = 25000
-LIBRE = O.10
+
+Estudiante=input("Eres estudiante? yes/no ").lower()
+
+match Estudiante:
+    case'yes':
+        Myu = 25000-(25000 * 0.15)
+        cupon=input("Tienes cupon? ingresa el codigo")                                      
+
+        if cupon== "LIBRO10":                                               
+            precio_final=Myu * 0.90                                                                                                                
+            print(f"Tu precio es {precio_final}")                                                                                          
+        else:
+            print(f"Tu precio es {Myu}")                                    
+    case 'no':
+        print("Tu precio total es de:25000")
+
+    case _:
+        print("Por favor responder yes or no")
+    
